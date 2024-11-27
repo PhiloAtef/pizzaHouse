4f12552267ba1550a70c0fa83ac06a54468063ff
PhiloAtef <philoatefdev@gmail.com>
2024-11-27 10:07:48 +0200

    eloquent model and MVC naming convention adjustment

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -3,22 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Pizza;
 
 class PizzaController extends Controller
 {
     public function index(){
-        $pizzas = [
-            ['type' => 'hawaiian', 'base' => 'cheesy crust'],
-            ['type' => 'volcano', 'base' => 'garlic crust'],
-            ['type' => 'veg supreme', 'base' => 'thin & crispy']
-        ];
-        return view('pizzas', [
+        
+        // $pizzas = Pizza::all();
+        // $pizzas = Pizza::orderBy("name","desc")->get();
+        // $pizzas = Pizza::where('type','hawaiian')->get();
+        $pizzas = Pizza::latest()->get();
+
+        return view('pizzas.index', [
             'pizzas' => $pizzas,
         ]);
     }
 
     public function show($id){
         //query data base with the $id
-        return view('details', ['id' => $id]);
+        return view('pizzas.show', ['id' => $id]);
+    }
+
+    public function create(){
+        return view('pizzas.create');
     }
 }
