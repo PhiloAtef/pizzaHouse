7435ac22a0e220052fbbe57c393f9e4394918048
PhiloAtef <philoatefdev@gmail.com>
2024-11-28 10:49:05 +0200

    playing around with sass

--- resources/views/pizzas/index.blade.php
+++ resources/views/pizzas/index.blade.php
@@ -1,20 +1,18 @@
 @extends('layouts.layout')
 
 @section('content')
-<div class="flex-center position-ref full-height">
-    <div class="content">
-        <div class="title m-b-md">
-            Pizzas List
-        </div>
-        @foreach ($pizzas as $pizza )
-        <div>
-        {{$pizza->name}} - {{$pizza->type}} - {{$pizza->base}}
-        </div>
-        @endforeach
-        <br>
-        <a href="/">Go to Home</a>
-        <br>
-        <a href="/pizzas/create">Add a New Pizza</a>
+<div class="wrapper pizza-index">
+    <h1>Pizza Orders</h1>
+    @foreach ($pizzas as $pizza )
+    <div class="pizza-item">
+        <img src="/img/pizza.png" alt="pizza icon">
+        <h4><a href="/pizzas/{{$pizza->id}}">{{$pizza->name}} </a></h4>
     </div>
+    @endforeach
+    <br>
+    <a href="/">Go to Home</a>
+    <br>
+    <a href="/pizzas/create">Add a New Pizza</a>
 </div>
+        
 @endsection
